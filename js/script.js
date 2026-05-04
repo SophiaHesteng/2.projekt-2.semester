68c66c0a72e171cd9805c79845b55b03a67f02d9
SophiaHesteng <sophes01@iba.dk>
2026-05-04 11:14:42 +0200

    4 variabler er skabt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 //console.log("Helle from Script"); Koblingen er sket
+
+//Variabler
+
+const startButton = document.querySelector(".startknap");
+
+const btns = document.querySelectorAll("button");
+
+const scenarios = document.querySelectorAll(".scenario");
+
+const feedback = document.querySelectorAll(".feedback");
