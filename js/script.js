066044131cb81423b98cf3eceb469d9a0554048e
SophiaHesteng <sophes01@iba.dk>
2026-05-05 09:38:52 +0200

    forgrening startet med hotspot

--- js/script.js
+++ js/script.js
@@ -8,16 +8,30 @@ const btns = document.querySelectorAll("button");
 
 const scenarios = document.querySelectorAll(".scenario");
 
-const feedback = document.querySelectorAll(".feedback");
+const feedbacks = document.querySelectorAll(".feedback");
 
 //Funktioner
 
 const nextScenario = (e) => {
         console.log(e.target.textContent);
-    switch(e.target.textContent) {
+    switch (e.target.textContent) {
         case "Start deling her":
-            //console.log("start deling her was clicked");
+            console.log("Start deling her was clicked");
             scenarios[1].classList.add("active");
+        break;
+
+        case "Du opretter et hotspot fra din mobil":
+            console.log("Du opretter et hotspot fra din mobil was clicked");
+            scenarios[1].classList.remove("active");
+            scenarios[2].classList.add("active");
+            
+        break;
+
+        case "Du deler linket, men uden at begrænse adgangen":
+            console.log("Du deler linket, men uden at begrænse adgangen was clicked");
+            scenarios[2].classList.remove("active");
+            scenarios[3].classList.add("active");
+        break;
     }
 }
 
