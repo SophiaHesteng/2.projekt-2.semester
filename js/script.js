e70c844a34843e679505c8d842807b05fe0787d6
SophiaHesteng <sophes01@iba.dk>
2026-05-05 12:42:27 +0200

    gode slutning på hotspot scenarie færdig

--- js/script.js
+++ js/script.js
@@ -18,21 +18,30 @@ const nextScenario = (e) => {
         case "Start deling her":
             console.log("Start deling her was clicked");
             scenarios[1].classList.add("active");
+            scenarios[3].classList.remove("active");
+            scenarios[4].classList.remove("active");
         break;
 
         case "Du opretter et hotspot fra din mobil":
             console.log("Du opretter et hotspot fra din mobil was clicked");
             scenarios[1].classList.remove("active");
             scenarios[2].classList.add("active");
-            
         break;
 
-        case "Du deler linket, men uden at begrænse adgangen":
-            console.log("Du deler linket, men uden at begrænse adgangen was clicked");
+        case "Du begrænser adgangen med en kode fra starten og sikrer at kun din gruppe har adgang":
+            console.log("Du begrænser adgangen med en kode fra starten og sikrer at kun din gruppe har adgang was clicked");
             scenarios[2].classList.remove("active");
             scenarios[3].classList.add("active");
+            scenarios[4].classList.remove("active");
         break;
-    }
+
+        case "Du deler filerne uden kontrol med kode":
+            console.log("Du deler filerne uden kontrol med kode was clicked");
+            scenarios[2].classList.remove("active");
+            scenarios[4].classList.add("active");
+            scenarios[3].classList.remove("active");
+        break;
+        }
 }
 
 
