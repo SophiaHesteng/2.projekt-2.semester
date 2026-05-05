26f80a0d14f797ab5868b3af4a564d7cdd80ed48
SophiaHesteng <sophes01@iba.dk>
2026-05-05 08:53:10 +0200

    section classes er stylet og js er påbegyndt

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@ const btns = document.querySelectorAll("button");
 const scenarios = document.querySelectorAll(".scenario");
 
 const feedback = document.querySelectorAll(".feedback");
+
+//Funktioner
+
+const nextScenario = (e) => {
+        console.log(e.target.textContent);
+    switch(e.target.textContent) {
+        case "Start deling her":
+            //console.log("start deling her was clicked");
+            scenarios[1].classList.add("active");
+    }
+}
+
+
+//EventListener
+ 
+for(const button of btns) {
+    button.addEventListener("click", nextScenario);
+}
